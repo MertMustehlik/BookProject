35893ddf2af3fa7a736b41d6805bfbf4cc4dbdfb
Mert Mustehlik <mertmustehlik@hotmail.com>
2022-04-12 17:15:28 +0300

    add-login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,64 @@
-@extends('layouts.app')
+@extends('front.temp')
+@section('title') Giriş Yap @endsection
+@section('css')@endsection
+@section('master')
+<!--start-breadcrumbs-->
+<div class="breadcrumbs">
+    <div class="container">
+        <div class="breadcrumbs-main">
+            <ol class="breadcrumb">
+                <li><a href="{{route('index')}}">Home</a></li>
+                <li class="active">Giriş Yap</li>
+            </ol>
+        </div>
+    </div>
+</div>
+<!--end-breadcrumbs-->
+<!--account-starts-->
+<div class="account">
+    <div class="container">
+        <div class="account-top heading">
+            <h2>Giriş Yap</h2>
+        </div>
+        <div class="account-main">
+            <div class="col-md-6 account-left">
+                <h3>Zaten kullanıcıyım</h3>
+                <div class="account-bottom">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <input id="email" class="@error('email') is-invalid @enderror" placeholder="Email" type="text" tabindex="3" value="{{ old('email') }}" name="email" required autofocus>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
 
-@section('content')
-<div class="container">
+                        <input id="password" placeholder="Şifre" type="password" tabindex="4" class="@error('password') is-invalid @enderror" name="password" required>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                        <div class="address">
+                            <a class="forgot" href="#">Forgot Your Password?</a>
+                            <input type="submit" value="Login">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-6 account-right account-left">
+                <h3>Yeni kullanıcı iseniz hemen kayıt olun</h3>
+                <p>By creating an account with our store, you will be able to move through the checkout process faster, store multiple shipping addresses, view and track your orders in your account and more.</p>
+                <a href="{{url('register')}}">Hesap oluştur</a>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+<!--account-end-->
+
+<!-- Basic login -->
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -18,9 +75,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -32,9 +89,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -58,9 +115,9 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                                 @endif
                             </div>
                         </div>
@@ -69,5 +126,6 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
+@section('js')@endsection
--- resources/views/front/datas/header.blade.php
+++ resources/views/front/datas/header.blade.php
@@ -1,67 +1,80 @@
 	<!--top-header-->
 	<div class="top-header">
-	    <div class="container">
-	        <div class="top-header-main">
-	            <div class="col-md-6 top-header-left">
-	                <div class="drop">
+		<div class="container">
+			<div class="top-header-main">
+				<div class="col-md-6 top-header-left">
+					<div class="drop">
+						@auth
+						<div class="box">
+							<a href="#" style="color:#fff; text-transform:capitalize;" >{{Illuminate\Support\Facades\Auth::user()->name}}</a>
+						</div>
+						<div class="box1">
+							<a href="{{ route('logout') }}" style="color:#fff;" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+								Çıkış Yap
+							</a>
+							<form id="logout-form" action="{{ route('logout') }}" method="POST">
+								@csrf
+							</form>
+						</div>
+						@endauth
 						@guest
-	                    <div class="box">
-	                        <a href="#">Giriş Yap</a>
-	                    </div>
-	                    <div class="box1">
-	                        <a href="{{url('/register')}}">Kayıt Ol</a>
-	                    </div>
+						<div class="box">
+							<a href="{{route('login')}}" style="color:#fff;">Giriş Yap</a>
+						</div>
+						<div class="box1">
+							<a href="{{route('register')}}" style="color:#fff;">Kayıt Ol</a>
+						</div>
 						@endguest
-	                    <div class="clearfix"></div>
-	                </div>
-	            </div>
-	            <div class="col-md-6 top-header-left">
-	                <div class="cart box_1">
-	                    <a href="checkout.html">
-	                        <div class="total">
-	                            <span class="simpleCart_total"></span>
-	                        </div>
-	                        <img src="{{asset('front/assets/images/cart-1.png')}}" alt="" />
-	                    </a>
-	                    <p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
-	                    <div class="clearfix"> </div>
-	                </div>
-	            </div>
-	            <div class="clearfix"></div>
-	        </div>
-	    </div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="col-md-6 top-header-left">
+					<div class="cart box_1">
+						<a href="checkout.html">
+							<div class="total">
+								<span class="simpleCart_total"></span>
+							</div>
+
+						</a>
+						<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
 	</div>
 	<!--top-header-->
 	<!--start-logo-->
 	<div class="logo">
-	    <a href="">
-	        <h1>Tryhard Books</h1>
-	    </a>
+		<a href="">
+			<h1>Tryhard Books</h1>
+		</a>
 	</div>
 	<!--start-logo-->
 	<!--bottom-header-->
 	<div class="header-bottom">
-	    <div class="container">
-	        <div class="header">
-	            <div class="col-md-9 header-left">
-	                <div class="top-nav">
-	                    <ul class="memenu skyblue">
+		<div class="container">
+			<div class="header">
+				<div class="col-md-9 header-left">
+					<div class="top-nav">
+						<ul class="memenu skyblue">
 							@foreach($categories as $category)
 							<li class=""><a href="#">{{$category->name}}</a></li>
 							@endforeach
-	                        
-	                    </ul>
-	                </div>
-	                <div class="clearfix"> </div>
-	            </div>
-	            <div class="col-md-3 header-right">
-	                <div class="search-bar">
-	                    <input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
-	                    <input type="submit" value="">
-	                </div>
-	            </div>
-	            <div class="clearfix"> </div>
-	        </div>
-	    </div>
+
+						</ul>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="col-md-3 header-right">
+					<div class="search-bar">
+						<input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+						<input type="submit" value="">
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
 	</div>
 	<!--bottom-header-->
